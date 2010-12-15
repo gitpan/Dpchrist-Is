d67c5acf3e1c3d2b80ca06203c4180a8a6e5e323
David Paul Christensen <dpchrist@holgerdanske.com>
2010-12-15 01:19:35 +0000

    Import of DPCHRIST/Dpchrist-Is-1.031 from CPAN.
    gitpan-cpan-distribution: Dpchrist-Is
    gitpan-cpan-version:      1.031
    gitpan-cpan-path:         DPCHRIST/Dpchrist-Is-1.031.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/Is.pm
+++ lib/Dpchrist/Is.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: Is.pm,v 1.30 2010-12-08 06:41:02 dpchrist Exp $
+# $Id: Is.pm,v 1.31 2010-12-15 00:39:26 dpchrist Exp $
 #######################################################################
 # package:
 #----------------------------------------------------------------------
@@ -34,7 +34,7 @@ our @EXPORT_OK = (
 
 our @EXPORT	= qw();
 
-our $VERSION	= sprintf "%d.%03d", q$Revision: 1.30 $ =~ /(\d+)/g;
+our $VERSION	= sprintf "%d.%03d", q$Revision: 1.31 $ =~ /(\d+)/g;
 
 #######################################################################
 # uses:
@@ -51,7 +51,7 @@ Dpchrist::Is - various type tests
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.30 $.
+This documentation describes module revision $Revision: 1.31 $.
 
 
 This is alpha test level software
